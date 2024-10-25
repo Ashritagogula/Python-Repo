cd0df7c436b6babe2a7c2593049e05f3d17fe5d8
Ashrita Gogula <ashritagogula12@gmail.com>
2024-10-25 13:18:17 +0530

    Uploading code: Lucky Character.py

---
+++ Lucky Character.py
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class LuckyCharacter{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        System.out.println(n.charAt(6));
+    }
+}
